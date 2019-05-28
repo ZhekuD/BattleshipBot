5eaa4c51aaca06034d1a713ae54f9276a529e46c
Eugene <zhekud@gmail.com>
2019-05-29 01:13:54 +0300

    add class Control with method shoot

--- rules.py
+++ rules.py
@@ -1,25 +1,58 @@
 class Player:
     def __init__(self):
         self.control = Control(self)
-        self.field = [[0] * 10] * 10
-        self.enemy_field = [[0] * 10] * 10
+        self.ships = Ships(self)
+        self.field = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
 
-    @staticmethod
-    def show_field(field):
-        for i in field:
+        self.enemy_field = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+
+    def show_field(self, field=True):
+        a = self.field if field else self.enemy_field
+        for i in a:
             print(i)
 
 
 class Ship:
     def __init__(self, length):
+        if 0 < length <= 4:
+            self.length = length
         self.horizontal = None
         self.x = None
         self.y = None
         self.alive = True
-        if 0 < length <= 4:
-            self.length = length
-        else:
-            raise AttributeError
+        self.health = self.length
+
+    def __repr__(self):
+        return f'S{self.length}'
+
+    def hit(self):
+        self.health -= 1
+        if not self.health:
+            self.alive = False
+            return self.alive
+        return self.alive
 
     def set_position(self, player, horizontal, x, y):
         self.horizontal = horizontal
@@ -30,7 +63,7 @@ class Ship:
         def wrap_creator(obj, *args):
             if args[0] >= 0 and args[1] >= 0:
                 try:
-                    obj.field[args[0]][args[1]] = 2
+                    obj.field[args[0]][args[1]] = 'x'
                 except IndexError:
                     pass
 
@@ -47,7 +80,7 @@ class Ship:
                 for i in range(self.length):    # Ставим сам горизонтальный корабль
                     if player.field[y][x + i]:
                         raise IndexError
-                    player.field[y][x + i] = 1
+                    player.field[y][x + i] = self
 
             else:
                 for i in range(3):              # Ставим ограничитель вокруг вертикального корабля
@@ -61,7 +94,7 @@ class Ship:
                 for i in range(self.length):    # Ставим сам вертикальный корабль
                     if player.field[y + i][x]:
                         raise IndexError
-                    player.field[y + i][x] = 1
+                    player.field[y + i][x] = self
 
         except IndexError:                      # Если корабль не поместился - востанавливаем изначальное поле
             player.field = field_copy
@@ -73,7 +106,7 @@ class Ship:
         return True
 
 
-class Control:
+class Ships:
     def __init__(self, player):
         self.player = player
         self.four_decker1 = Ship(4)
@@ -99,6 +132,31 @@ class Control:
 
                     self.player.show_field()
                     print('\n')
+
+
+class Control:
+    def __init__(self, player):
+        self.player = player
+
+    def shoot(self, enemy, x, y):
+        enemy_health = None
+        if isinstance(enemy.field[y][x], Ship):
+            enemy_ship = enemy.field[y][x]
+            enemy_health = enemy_ship.hit()
+            enemy.field[y][x] = 2
+            self.player.enemy_field[y][x] = 'hit'
+            print('Hit!')
+
+            if not enemy_health:
+                x, y = enemy_ship.x, enemy_ship.y
+                length = enemy_ship.length
+                pass
+
+        else:
+            self.player.enemy_field[y][x] = 'miss'
+
+        self.player.show_field(False)
+        return enemy_health
 
 
 if __name__ == '__main__':
